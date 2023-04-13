811e836a0bfd6a00144d2692d8667f311ab0f218
ilse31 <ilhamprazsetyo33@gmail.com>
2023-04-13 20:13:53 +0700

    add new config

---
+++ webpack.config.ts
@@ -0,0 +1,15 @@
+import { Configuration } from "webpack";
+import path from "path";
+
+const config = (): Configuration => {
+  return {
+    mode: "production",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+  };
+};
+
+export default config;
