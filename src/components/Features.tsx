9695de2713d4f537938b2f90523a06ce15fdf69d
Ilham Prasetya <ilham.prasetya@nawadata.id>
2023-06-07 14:39:56 +0700

    testiing

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,7 +8,10 @@ const Features = (props: Props) => {
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
       consectetur atque facilis ipsam. Provident, expedita. Accusantium tempore
       velit eligendi non asperiores. Iusto, cumque animi vitae praesentium
-      provident atque dicta veritatis.
+      provident atque dicta veritatis. Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Molestiae doloremque excepturi explicabo, sed voluptates
+      possimus fugit quasi odit perferendis provident, inventore placeat
+      laudantium omnis porro, cumque rerum! Labore, voluptate modi!
     </div>
   );
 };
