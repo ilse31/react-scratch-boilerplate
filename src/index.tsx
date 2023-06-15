17ff85e2f48301782479645f94163363bc6d699f
Ilham Prasetya <ilham.prasetya@nawadata.id>
2023-06-15 10:52:21 +0700

    change sw

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import * as serviceWorkerRegistration from "./registerServiceWorker";
+import { registerServiceWorker } from "./registerServiceWorker";
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
 console.log("index.tsx", process.env.PUBLIC_URL);
-serviceWorkerRegistration.register();
+registerServiceWorker();
