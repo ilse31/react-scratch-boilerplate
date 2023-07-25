4bad77c679379d92f20986211b8561db2cc5f715
Ilham Prasetya <58392119+ilse31@users.noreply.github.com>
2023-07-25 14:52:33 +0700

    Update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,37 +1,53 @@
-const CACHE_NAME = "my-pwa-cache";
-const urlsToCache = ["/", "/index.html"];
-const maximumFileSizeToCacheInBytes = 5 * 1024 * 1024; // Ubah ukuran sesuai kebutuhan Anda (contoh: 5MB)
+const CACHE_NAME = "my-pwa-cache-v1"; // Ubah versi cache saat ada perubahan
+const urlsToCache = ["/", "/index.html", "/styles.css", "/app.js", "/image.jpg"];
 
-self.addEventListener("install", (event: any) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(
-        urlsToCache.filter((url) => {
+        urlsToCache.map((url) => {
           return fetch(url)
             .then((response) => {
-              if (
-                !response ||
-                response.status !== 200 ||
-                response.type !== "basic"
-              ) {
-                return false;
+              if (!response || response.status !== 200 || response.type !== "basic") {
+                return null;
               }
 
               const contentLength = response.headers.get("content-length");
-              if (
-                contentLength &&
-                parseInt(contentLength, 10) > maximumFileSizeToCacheInBytes
-              ) {
-                return false;
+              if (contentLength && parseInt(contentLength, 10) > maximumFileSizeToCacheInBytes) {
+                return null;
               }
 
-              return true;
+              cache.put(url, response.clone()); // Menyimpan respons di cache
+              return url;
             })
             .catch(() => {
-              return false;
+              return null;
             });
         })
       );
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Menghapus cache lama saat aktivasi
+          }
+        })
+      );
+    })
+  );
+});
+
+// Menggunakan strategi cache-first untuk permintaan sumber daya
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
